ae40b4606fdde642ca25f9916fac80c1f9140baa
eldoogy <ldoogy@gmail.com>
2016-08-10 23:01:03 -0700

    Initial Commit

---
+++ EEIOKitListenerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EEIOKitListenerSample
+//
+//  Created by Eldad Eilam on 8/10/16.
+//  Copyright © 2016 Eldad Eilam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EEIOKitListenerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EEIOKitListenerSample
+//
+//  Created by Eldad Eilam on 8/10/16.
+//  Copyright © 2016 Eldad Eilam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
